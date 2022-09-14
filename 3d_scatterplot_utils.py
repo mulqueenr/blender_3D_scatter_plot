e968eeeeec5260d0b54412f5ba160c2b0007651c
Ryan Mulqueen <mulqueenr@gmail.com>
2022-09-14 16:55:08 -0500

    Update 3d_scatterplot_utils.py

--- 3d_scatterplot_utils.py
+++ 3d_scatterplot_utils.py
@@ -51,3 +51,61 @@ def make_master_shader():
   mat_links.new(bpy.data.materials["mymat"].node_tree.nodes["Mix Shader"].outputs[0], bpy.data.materials["mymat"].node_tree.nodes["Mix Shader.001"].inputs[1])
   mat_links.new(bpy.data.materials["mymat"].node_tree.nodes["Light Path"].outputs[1], bpy.data.materials["mymat"].node_tree.nodes["Mix Shader.001"].inputs[0])
   mat_links.new(bpy.data.materials["mymat"].node_tree.nodes["Mix Shader.001"].outputs[0], bpy.data.materials["mymat"].node_tree.nodes["Material Output"].inputs[0])
+
+  
+def set_render_and_scene():
+  """Automate setting up high quality render and scene."""
+  #set up render engine and scene
+  bpy.context.scene.render.engine="CYCLES" #set render engine to CYCLES
+  bpy.data.scenes["Scene"].cycles.denoiser="NLM" #set denoiser for render
+  bpy.data.scenes["Scene"].cycles.samples=512 #this is a whole lotta sampling
+  bpy.context.scene.render.image_settings.color_depth = '16' #more color channels!
+  bpy.context.scene.render.resolution_x = 3840 #up the resolution
+  bpy.context.scene.render.resolution_y = 2160
+  bpy.data.objects["Sphere"].hide_render = True # hide sphere in render
+  bpy.data.objects["Sphere"].hide_viewport=True
+  bpy.data.lights["Light"].energy = 100000 # increase light wattage
+  bpy.data.lights["Light"].shadow_soft_size= 1
+  bpy.data.objects["Light"].location=(5,-5,10) #location and rotation i deteremined manually and just set up here for convenience
+
+def set_up_stage():
+  """Set up stage by cutting up the default cube vertices and smoothing it."""
+  obj_cube=bpy.data.objects["Cube"]
+  obj_cube.scale=(30,30,30) #scale up the cube
+  #this is to cut out a vertex to make an open box
+  bpy.context.view_layer.objects.active = obj_cube
+  bpy.ops.object.mode_set(mode='EDIT')
+  bpy.ops.mesh.select_mode(type="VERT")  # Switch to edge select mode
+  bm = bmesh.from_edit_mesh(obj_cube.data)  # Create bmesh object for easy mesh evaluation
+  bm.verts.ensure_lookup_table()
+  bm.verts.remove(bm.verts[2]) # Write the mesh back
+  bmesh.update_edit_mesh(obj_cube.data)  # Update the mesh in edit mode
+  bpy.ops.object.mode_set(mode='OBJECT') #switch back to object mode when done
+  bpy.ops.object.modifier_add(type='SUBSURF') #make it smooth
+  bpy.data.objects["Cube"].modifiers["Subdivision"].render_levels=6
+  bpy.data.objects["Cube"].location=(-4,4.3,17.725) #change the location for more dramatic shadows
+  
+  
+def set_camera():
+  """Move the camera and rotate."""
+  bpy.data.objects["Camera"].location=(34.61997604370117, -40.53969955444336, 25.66326904296875)
+  bpy.data.objects["Camera"].rotation_euler=(1.1093189716339111, 0.0, 0.8149281740188599)
+
+  
+def add_data_point(input_dat):
+    """Copy data points and link them to the master spheres. 
+    Also places the copies into nice cluster named collections for easier navigation."""
+    line=input_dat
+    line=line.replace('\n','')
+    l=line.split('\t')
+    #print(line)
+    x=float(l[2]) #location of spheres
+    y=float(l[3])
+    z=float(l[4])
+    name=str(l[1])
+    clust=str(l[0])
+    my_new_obj = bpy.data.objects.new(name,master_sphere[clust])
+    my_new_obj.location = (x,y,z)       
+    my_new_obj.hide_viewport=False
+    my_new_obj.hide_render=False
+    bpy.data.collections[clust].objects.link(my_new_obj)
